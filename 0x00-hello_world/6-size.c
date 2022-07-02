a5e255fb644fe30f7750b4e9c6df4211b0acf4a9
Akolade <emmakolade@gmail.com>
2022-07-02 16:01:40 -0700

    low level

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: return always 0
+ */
+int main(void)
+{
+	printf("The Size of a char: %d.\n", sizeof(char));
+	printf("The Size of an int: %d.\n", sizeof(int));
+	printf("The Size of a long int: %d.\n", sizeof(long int));
+	printf("The Size of a long long int %d.\n", sizeof(long long int));
+	printf("The Size of a float %d.\n", sizeof (float));
+	return (0);
+}
