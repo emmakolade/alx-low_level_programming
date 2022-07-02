b6df130855e4a6a70bdff2a9c7508ef2b5927a6f
Akolade <emmakolade@gmail.com>
2022-07-02 14:55:49 -0700

    low level

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * this is hellp print f task
+ *
+ * and this programm will return 
+ *
+ * return: return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
