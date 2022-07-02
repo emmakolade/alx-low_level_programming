c6e8f86c2b9b5c298dc9fddd84e0d928d01bbc11
Akolade <emmakolade@gmail.com>
2022-07-02 15:17:52 -0700

    low level

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 /**
-*main - Entry point
-*
-*this is printf task
-* 
-*and this programm will return 
-*
-*Return: return 0 
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
