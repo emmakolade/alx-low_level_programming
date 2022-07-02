5d00988e03e4f15c853c52429b47eb52d7c22947
Akolade <emmakolade@gmail.com>
2022-07-02 16:07:12 -0700

    low level

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("The Size of a char: %d.\n", sizeof(char));
-	printf("The Size of an int: %d.\n", sizeof(int));
-	printf("The Size of a long int: %d.\n", sizeof(long int));
-	printf("The Size of a long long int %d.\n", sizeof(long long int));
-	printf("The Size of a float %d.\n", sizeof (float));
+	printf("The Size of a char: %d byte(s)\n", sizeof(char));
+	printf("The Size of an int: %d byte(s)\n", sizeof(int));
+	printf("The Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("The Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("The Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
