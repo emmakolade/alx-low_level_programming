4a72c4059771e4584404470808be4d3a12a6957d
Akolade <emmakolade@gmail.com>
2022-07-03 10:37:58 -0700

    this was tough

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stulib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+int ch;
+for (ch = 48; ch <= 57; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
