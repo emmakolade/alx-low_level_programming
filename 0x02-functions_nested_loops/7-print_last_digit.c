de4e45e1e8c208d9edac986e7e4973b583828af2
Akolade <emmakolade@gmail.com>
2022-07-11 15:59:43 -0700

    commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_last_digit -> prints the last digit of a number 
- * @n: the int to extract the last digit from 
+ * print_last_digit -> prints the last digit of a number
+ * @n: the int to extract the last digit from
  * Return: always return 0
  */
 int print_last_digit(int n)
