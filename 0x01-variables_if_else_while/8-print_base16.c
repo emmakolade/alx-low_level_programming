7be86f5c0ab862c6d0800ebba58b8aa162529f1e
Akolade <emmakolade@gmail.com>
2022-07-11 12:10:06 -0700

    commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,7 +11,7 @@ int main(void)
 {
 char ch;
 int n;
-for (n = 48; n <=57; n++)
+for (n = 48; n <= 57; n++)
 {
 putchar(n);
 }
@@ -19,6 +19,6 @@ for (ch = 'a'; ch <= 'f'; ch++)
 {
 putchar(ch);
 }
-putchar(10);
+putchar('\n');
 return (0);
 }
