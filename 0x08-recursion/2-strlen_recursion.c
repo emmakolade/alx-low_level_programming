15315aaca7a90f199dbe2002ebcae4c153c3a409
Akolade <emmakolade@gmail.com>
2022-07-23 02:46:56 -0700

    returns the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the lenght of a string
+ * @s: the string to be located
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+{
+	return (0);
+}
+	else
+{	return(_strlen_recursion(s + 1);
+}
+}
