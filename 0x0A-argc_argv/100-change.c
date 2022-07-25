921b1d1b4c3ac87f565f75992ebb1f6712c3ce35
Akolade <emmakolade@gmail.com>
2022-07-25 13:33:59 -0700

    coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,34 +9,28 @@
  */
 int main(int argc, char argv[])
 {
-	int c, coins = 0;
+	int cents, coins = 0;
 
-	if (argc != 2)
+	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	c = atoi(argv[1]);
-	if (c < 0)
+	cents = atoi(argv[1]);
+	while (cents > 0)
 	{
-		printf("0\n");
-		return (0);
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		ncoins += 1; 
 	}
-	for (; c >= 0;)
-	{
-		if (c >= 25)
-			c -= 25;
-		else if (c >= 10)
-			c -= 10;
-		else if (c >= 5)
-			c -= 5;
-		else if (c >= 2)
-			c -= 2;
-		else if (c >= 1)
-			c -= 1;
-		else 
-			break;
-	}
-	printf("%d\n", coins);
+	printf("%d\n", ncoins);
 	return (0);
 }
