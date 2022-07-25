cca7c37690a42635fefbfd23d87fe81753c33207
Akolade <emmakolade@gmail.com>
2022-07-25 13:26:36 -0700

    coins

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	str = argv[i];
 	for (b = 0; str[b] != '\0'; b++)
 {
-	if (str[b] < 0 || str[b] > 9)
+	if (str[b] < 48 || str[b] > 57)
 {
 	printf("Error\n");
 	return (1);
