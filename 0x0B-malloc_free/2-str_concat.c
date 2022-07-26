67a8987feb93d48d045cc2c1977617984eff5866
Akolade <emmakolade@gmail.com>
2022-07-26 05:23:38 -0700

    function that returns a pointer to a 2 dimensional array of integers

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: The sting to be concatenate upon
+ * @s2: the string to be concatnated to s1
+ *
+ * Return: if concatenation fail - NULL
+ */
+
