dff898a07490884481185d4ff865e7a0246a89f8
Akolade <emmakolade@gmail.com>
2022-07-28 17:01:42 -0700

    correction

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 10; n++)
+	for (n = 0; n < 10; n++)
 {
-	putchar(n % 10 + '0');
+	putchar((n % 10)+ '0');
 }
-	putchar("\n");
+	putchar('\n');
 
 	return (0);
 }
