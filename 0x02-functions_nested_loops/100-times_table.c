e0a89bead26e05931c4e8c72a6ba4c704d511ae9
Akolade <emmakolade@gmail.com>
2022-07-31 01:55:09 -0700

    correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,46 +1,50 @@
 #include "main.h"
 /**
- * print_times_table - -prints the n times table, stating with 0
- * @n: number of the times table 
+ * print_times_table - print times table
+ * @n: number to be printed
  */
 void print_times_table(int n)
 {
-int i, j, k;
-if (n >= 0 && n <= 15)
-{
-for (i = 0; i <= n; i++)
-{
-for (j = 0; j <= n; j++)
-{
-k = j * i;
-if (j == 0)
-{
-_putchar(k + '0');
-}
-else if (k < 10 && j != 0)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-_putchar(k + '0');
-}
-else if (k >= 10 && k < 100)
-{_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
-}
-else if (k >= 100)
-{_putchar(',');
-_putchar(' ');
-_putchar((k / 100) + '0');
-_putchar(((k / 10) % 10) + '0');
-_putchar((k % 10) + '0');
-}
-}
-_putchar('\n');
-}
-}
+	int x, y, z;
+
+	if (n < 0 || n > 15)
+	{
+		for (x = 0; x <= 0; x++)
+		{
+			for (y = 0; y <= 0; y++)
+			{
+				z = y * x;
+
+				if (y == 0)
+				{
+					_putchar(z + '0');
+				}
+				else if (z < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(z + '0');
+				}
+				else if (z >= 10 && z < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((z / 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
