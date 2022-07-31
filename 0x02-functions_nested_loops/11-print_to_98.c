d2a4e8f6ee8602e397c8c04fca63ecd5ec50dc2c
Akolade <emmakolade@gmail.com>
2022-07-31 00:44:49 -0700

    correction

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,30 +2,11 @@
 #include "main.h"
 /**
  * print_to_98 - prints to 98
- * @n: the strating point 
+ * @n: the strating point
  */
 void print_to_98(int n);
 {
-if (n <= 98)
-{
-while (n <= 98)
-{
-if (n == 98)
-printf("%d\n", n);
-else 
-printf("%d, ", n);
-n = n + 1;
-}
-}
-else if (n > 98)
-{
-while (n >= 98)
-{
-if (n == 98)
-printd("%d\n", n);
-else 
-printf("%d, ", n);
-n = n - 1;
-}
-}
+	for (n = 0; n <= 98; n++)
+		_putchar(n + '0');
+		_putchar(',');
 }
