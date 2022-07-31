1fe54beb70468cae32dc849f1dc0d2a9c26e66f7
Akolade <emmakolade@gmail.com>
2022-07-30 18:33:24 -0700

    correction

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,17 @@
 #include "main.h"
 /**
- * add -> adds two number 
+ * add -> adds two number
  * @x : param1
  * @y : param2
  * Return: x + y
  */
 int add(int x, int y)
 {
-return (x + y);
+	int x;
+	int y;
+	int res;
+
+	res = add(x, y);
+		_putchar(res);
+return (res);
 }
