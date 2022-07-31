e8459dc1f9dc7a142622bfd0d0ea4147cb32bb10
Akolade <emmakolade@gmail.com>
2022-07-31 03:27:59 -0700

    correction

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,6 @@
 /**
  * print_most_numbers - a function that prints 0-9.
  * Do not print 2 and 4.
- * Return: 0
  */
 void print_most_numbers(void)
 {
@@ -14,4 +13,6 @@ if (i != 2 && i != 4)
 _putchar(i + '0');
 }
 _putchar('\n');
+
+return (0);
 }
