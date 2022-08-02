0ec0fc59796080b6309091c1af8dd724aa8e495d
emmakolade <43618115+emmakolade@users.noreply.github.com>
2022-08-03 00:50:43 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,13 +10,10 @@
 
 void print_array(int *a, int n)
 {
-int i = 0;
-
-for (; i < n; i++)
-{
-printf("%d", *(a + i));
-
-if (i != (n - 1))
-printf(", ");
-}										printf("\n");
+  int i;
+  
+  for (i = 0; i < n; i++)
+  {
+    printf("elements of an arra of integers: %d, \n", a[i]);
+  }
 }
