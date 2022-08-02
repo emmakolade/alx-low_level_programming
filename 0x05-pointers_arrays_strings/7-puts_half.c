633b83a607354cf10a28bc75a20ae0af2955443e
emmakolade <43618115+emmakolade@users.noreply.github.com>
2022-08-03 00:36:06 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,17 +7,30 @@
  */
 void puts_half(char *str)
 {
-	int count = 0, i;
-	while (count >= 0)
-{
-	if (str[count] == '\0')
-	break;										count++;
-}
-	if (count % 2 == 1)
-	i = count / 2;
+	int i;
+	int k;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++
+	}
+
+	if (i % 2 == 1)
+	{
+		k = (i - 1) / 2;
+		k += 1;
+	}
 	else
-	i = (count - 1) / 2;
-	for (i++; i < count; i++)
-	_putchar(str[i]);
+	{
+		k = i / 2;
+	}
+
+	for (; k < i; k++)
+	{
+		_putchar(str[k]);
+	}
+
 	_putchar('\n');
 }
