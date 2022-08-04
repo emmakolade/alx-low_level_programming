91ef6fd92b854debc0870d127b4013d31b630df1
emmakolade <43618115+emmakolade@users.noreply.github.com>
2022-08-04 12:16:28 +0100

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,7 +12,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(ap, args);
 
-	int i = 0, sum = 0;
+	unsigned int i = 0, sum = 0;
 
 	for (i = 0; i < args; i++)
 		sum += va_args(ap, int);
