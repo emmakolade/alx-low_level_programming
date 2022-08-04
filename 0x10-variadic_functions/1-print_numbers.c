b2804a0e80b3510b6d9565e64f3459133d212e75
emmakolade <43618115+emmakolade@users.noreply.github.com>
2022-08-04 13:20:10 +0100

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,5 +1,6 @@
 #include "variadic_functions.h"
 #include <stdio.h>
+#include <stdarg.h>
 /**
  * print_numbers - function that prints numbers
  * @separator: the string to be printed btw numbers
@@ -17,9 +18,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		pn = va_arg(ap, const unsigned int);
+		pn = va_arg(ap, int);
 		printf("%d\n", pn);
 	}
-	
 	va_end(ap);
 }
