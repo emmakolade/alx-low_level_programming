c64d425fc018d5b52d8222fad68625caa34941b3
Akolade <emmakolade@gmail.com>
2022-08-11 07:11:55 -0700

    1

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ *  print_list - prints all the element of list_t list
+ *  @h: the string.
+ *
+ *  Return: no of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int node = 0;
+
+	if (h == NULL)
+		printf("[0] (nil)");
+	else if (h != '\0')
+	{
+		printf("list_t";
+	}
+	return (node);
+}
