bcd23d6a31636c0e8334182ccf28c42e13b3c6b2
emmakolade <43618115+emmakolade@users.noreply.github.com>
2022-08-11 22:37:02 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,16 +14,17 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nil\n");
-			h = h->next;
-			node++;
-			continue
-
+		}
 		else
+		{
 			printf("[%d] %s\n", h->len, h->str);
+		}
 
 		h = h->next;
 		node++;
 	}
+
 	return (node);
 }
