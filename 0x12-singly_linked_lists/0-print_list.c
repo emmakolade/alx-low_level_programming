44ffc5b4573479dbe8848f84569341883d967e3e
Akolade <emmakolade@gmail.com>
2022-08-11 08:08:26 -0700

    1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,12 +13,12 @@ size_t print_list(const list_t *h)
 {
 	int node = 0;
 
-	if (h->str == NULL)
-		printf("[0] (nil)");
-
-	for (; h-> != '\0'; node++)
+	for (; h->next != '\0'; node++)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h-> str == NULL)
+			printf("[0] (nil\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 	}
 	return (node);
 }
