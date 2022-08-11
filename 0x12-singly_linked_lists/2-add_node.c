19c9aab34bb52e856964121eadf7960ef1e775db
emmakolade <43618115+emmakolade@users.noreply.github.com>
2022-08-12 00:02:31 +0100

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
@@ -15,6 +14,9 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 	size_t node;
+
+	if (head == NULL)
+		return (NULL);
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
