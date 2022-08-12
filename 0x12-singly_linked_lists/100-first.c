8cb25215f500a7bbbe6bca497c5452afbc146903
Akolade <emmakolade@gmail.com>
2022-08-11 17:13:22 -0700

    executes a function before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,7 +7,7 @@
 void start(void) __attribute__ ((constructor));
 
 /**implementation */
-void start (void)
+void start(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
