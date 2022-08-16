8f3f4775752c2572c8ee090c66c7297be4b50412
emmakolade <43618115+emmakolade@users.noreply.github.com>
2022-08-16 19:43:09 +0100

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,20 +3,18 @@
 /**
  * free_listint2 - Frees a listint_t list.
  * @head: A pointer to the address of the
- *        head of the listint_t list.
+ * Return: 0
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp
-
 	if (head == NULL)
 		return;
 
 	while (*head)
 	{
-		tmp = (*head)->next;
+
 		free(*head);
-		*head = tmp;
+		*head = (*head)->next;
 	}
-	head = NULL,
+	*head = NULL,
 }
