ea0d8c3cca18876b8a769040a207554ce3a8ac87
emmakolade <emmakolade@gmail.com>
2022-08-16 20:23:44 +0100

    locates a given node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a list
+ * @index: index of the node starting at 0
+ * @head: pointer to the head of the list
+ * Return: NULL if the node doesnt exist 
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int node;
+	
+	for (node = 0; node < index; node++)
+	{
+		if (head == NULL)
+			return (NULL);
+		
+		head = head->next;
+	}
+	return (head);
+}
