9155f1d3342728940b10accc2b155dab4f0b8981
emmakolade <43618115+emmakolade@users.noreply.github.com>
2022-08-18 14:49:03 +0100

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,10 +12,12 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int bit;
-	
-	bit = (( *n >> index) | 1);
+
 	if (index > 64)
 		return (-1);
-	else
+	else if (!((*n >> index) & 1))
+	{
+		*n += 1 << index;
+	}
 		return (1);
 }
