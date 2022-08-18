b1fe94bd48ab065307c179645de04f9ec5012434
emmakolade <emmakolade@gmail.com>
2022-08-18 15:19:22 +0100

    Counts the number of bits flipped

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,11 +10,12 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int xor, n_bits;
+	unsigned long int xor;
+	unsigned int n_bits = 0;
 
 	xor = n ^ m;
-	n_bits = 0;
 
+	while (xor)
 	{
 		n_bits += (xor & 1);
 		xor >> = 1;
