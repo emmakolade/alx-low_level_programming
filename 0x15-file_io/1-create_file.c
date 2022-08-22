3aeb63412f0254358b855ed7688dc5c31d9f425b
emmakolade <emmakolade@gmail.com>
2022-08-22 16:58:26 +0100

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,21 +13,22 @@
 int create_file(const char *filename, char *text_content)
 {
 	int o, w, let = 0;
-	
+
 	if (filename == NULL)
 		return (-1);
-	
+
 	if (text_content != NULL)
 	{
-		for (let = 0, text_content[let]; let++;)
+		for (let = 0, text_content[let];)
+			let++;
 	}
 
 	o = open (filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write (o, text_content, letter);
-	
+
 	if (o == -1 || w == -1)
 		return (-1);
-	
+
 	close(o);
 	
 	return (1);
