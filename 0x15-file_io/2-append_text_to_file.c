6ac79ca08775ca02c001631a075352868976734a
emmakolade <emmakolade@gmail.com>
2022-08-22 17:12:51 +0100

    Append text to File

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,26 +15,26 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd;
 	int let;
 	int rwr;
-	
+
 	if (!filename)
 		return (-1);
-	
+
 	fd = open(filename, O_WRONLY | O_APPEND);
-	
+
 	if (fd == -1)
 		return (-1);
-	
+
 	if (text_content)
 	{
 		for (let = 0; text_content[let]; let++)
 			;
 		rwr = write(fd, text_content, let);
-		
+
 		if (rwr == -1)
 			return (-1);
 	}
-	
+
 	close(fd);
-	
+
 	return (1);
 }
